8f210831d2fcd2cd495828df56dd1b51b966666f
Albert Tang <tangalbert919@gmail.com>
2017-05-12 08:30:08 -0500

    Add Swift Lab and Python Lab 2

---
+++ Python/Lab02/main/__init__.py
@@ -0,0 +1,15 @@
+# This is a single-line comment. It starts with the "#" sign.
+"""
+This is a multi-line comment.
+Multi-line comments require triple quotation marks.
+"""
+# New variables can be declared without having to specify it being a number or string.
+number = 5
+epic = "Life"
+print(number)
+print(epic)
+# "If" and "else" statements don't need curly brackets.
+if number == 4 | number <= 6:
+    print(number)
+else:
+    print("The number is not 4, nor is it less than or equal to 6.")
---
+++ Ruby/README.md
@@ -0,0 +1,3 @@
+# Ruby Labs
+----------------
+Coming soon.
