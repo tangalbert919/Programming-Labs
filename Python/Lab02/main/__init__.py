eaefc3a080d0dba020d854c8794cc64e24b8c5ae
Albert Tang <tangalbert919@gmail.com>
2017-05-17 11:43:16 -0500

    Add Python Lab 2

--- Python/Lab02/main/__init__.py
+++ Python/Lab02/main/__init__.py
@@ -4,12 +4,13 @@ This is a multi-line comment.
 Multi-line comments require triple quotation marks.
 """
 # New variables can be declared without having to specify it being a number or string.
-number = 5
+number = 7
 epic = "Life"
 print(number)
 print(epic)
-# "If" and "else" statements don't need curly brackets.
+# "If" and "else" statements don't need curly brackets. Blocks of code are defined with whitespaces.
 if number == 4 | number <= 6:
     print(number)
 else:
     print("The number is not 4, nor is it less than or equal to 6.")
+    print("Please make the number either 4 or less than or equal to 6.")
